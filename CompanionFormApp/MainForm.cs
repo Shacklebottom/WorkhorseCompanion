50990ca3b6fce57768f66e887d0a80aceb845868
Shacklebottom <last0illusion@gmail.com>
2024-02-13 23:58:08 -0600

    Organized the MainForm with regions

--- CompanionFormApp/MainForm.cs
+++ CompanionFormApp/MainForm.cs
@@ -18,6 +18,7 @@ namespace CompanionFormApp
 
         private List<Ticket> ProjectTickets = new();
 
+        //Constructor
         public MainForm()
         {
             InitializeComponent();
@@ -28,6 +29,7 @@ namespace CompanionFormApp
             }
         }
 
+        #region PopulateUI Elements
         private void PopulateTickets()
         {
             if (ProjectTickets.Count == 0) return;
@@ -63,7 +65,9 @@ namespace CompanionFormApp
 
             btnEditTicket.Enabled = true;
         }
+        #endregion
 
+        #region Tool Strip Menu Items
         private void tsmiAddProject_clicked(object sender, EventArgs e)
         {
             NewProjectForm newProjectForm = new NewProjectForm();
@@ -93,6 +97,64 @@ namespace CompanionFormApp
             }
         }
 
+        private void tsmiEditProject_clicked(object sender, EventArgs e)
+        {
+            if (CurrentProject.Folder == null)
+            {
+                MessageBox.Show("Please select a project.");
+
+                return;
+            }
+
+            EditProjectForm editProjectForm = new EditProjectForm(CurrentProject);
+
+            editProjectForm.ShowDialog();
+        }
+
+        private void tsmiOpenSolution_clicked(object sender, EventArgs e)
+        {
+            if (CurrentProject.Solution == null)
+            {
+                MessageBox.Show("No Project Solution set. Please try again.");
+
+                return;
+            }
+
+            var visualStudioDir = "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe";
+
+            var solutionPath = $@"{CurrentProject.Solution}";
+
+            Process.Start(visualStudioDir, solutionPath);
+        }
+
+        private void tsmiOpenGitBash_clicked(object sender, EventArgs e)
+        {
+            if (CurrentProject.Folder == null)
+            {
+                MessageBox.Show("No Project Folder set. Please try again.");
+
+                return;
+            }
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = "C:\\Program Files\\Git\\git-bash.exe",
+                WorkingDirectory = $"{CurrentProject.Folder}"
+            };
+
+            Process.Start(processStartInfo);
+        }
+        #endregion
+
+        #region ListBox & Its Buttons
+        private void lstbxProjectTickets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var ticketIndex = lstbxProjectTickets.SelectedIndex;
+
+            var ticket = ProjectTickets[ticketIndex];
+
+            PopulateTicketDetails(ticket);
+        }
+
         private void btnNewTicket_clicked(object sender, EventArgs e)
         {
             if (CurrentProject.Folder == null)
@@ -114,58 +176,18 @@ namespace CompanionFormApp
             PopulateTickets();
         }
 
-        private void lstbxProjectTickets_SelectedIndexChanged(object sender, EventArgs e)
+        private void btnAllTickets_clicked(object sender, EventArgs e)
         {
-            var ticketIndex = lstbxProjectTickets.SelectedIndex;
+            if (CurrentProject.Tickets.Count == 0) return;
 
-            var ticket = ProjectTickets[ticketIndex];
+            lstbxProjectTickets.Items.Clear();
 
-            PopulateTicketDetails(ticket);
-        }
+            ProjectTickets = CurrentProject.Tickets;
 
-        private void btnEditTicket_clicked(object sender, EventArgs e)
-        {
-            var ticketIndex = lstbxProjectTickets.SelectedIndex;
-
-            var ticket = ProjectTickets[ticketIndex];
-
-            EditTicketForm editTicketForm = new EditTicketForm(CurrentProject, ticket);
-
-            if (editTicketForm.ShowDialog() == DialogResult.Cancel) return;
-
-            CurrentProject = editTicketForm.CurrentProject;
-
-            PopulateTicketDetails(ProjectTickets[ticketIndex]);
-        }
-
-        private void tsmiOpenSolution_clicked(object sender, EventArgs e)
-        {
-            if (CurrentProject.Solution == null)
+            foreach (var ticket in ProjectTickets)
             {
-                MessageBox.Show("No Project Solution set. Please try again.");
-
-                return;
+                lstbxProjectTickets.Items.Add(ticket.Name);
             }
-
-            var visualStudioDir = "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe";
-
-            var solutionPath = $@"{CurrentProject.Solution}";
-
-            Process.Start(visualStudioDir, solutionPath);
-        }
-
-        private void tsmiEditProject_clicked(object sender, EventArgs e)
-        {
-            if (CurrentProject.Folder == null)
-            {
-                MessageBox.Show("Please select a project.");
-
-                return;
-            }
-
-            EditProjectForm editProjectForm = new EditProjectForm(CurrentProject);
-
-            editProjectForm.ShowDialog();
         }
 
         private void btnActiveTickets_clicked(object sender, EventArgs e)
@@ -196,23 +218,23 @@ namespace CompanionFormApp
             }
         }
 
-        private void tsmiOpenGitBash_clicked(object sender, EventArgs e)
+        private void btnEditTicket_clicked(object sender, EventArgs e)
         {
-            if (CurrentProject.Folder == null)
-            {
-                MessageBox.Show("No Project Folder set. Please try again.");
+            var ticketIndex = lstbxProjectTickets.SelectedIndex;
 
-                return;
-            }
-            ProcessStartInfo processStartInfo = new ProcessStartInfo()
-            {
-                FileName = "C:\\Program Files\\Git\\git-bash.exe",
-                WorkingDirectory = $"{CurrentProject.Folder}"
-            };
+            var ticket = ProjectTickets[ticketIndex];
 
-            Process.Start(processStartInfo);
+            EditTicketForm editTicketForm = new EditTicketForm(CurrentProject, ticket);
+
+            if (editTicketForm.ShowDialog() == DialogResult.Cancel) return;
+
+            CurrentProject = editTicketForm.CurrentProject;
+
+            PopulateTicketDetails(ProjectTickets[ticketIndex]);
         }
+        #endregion
 
+        #region Git Bash Panel
         private void btnCommitProject_clicked(object sender, EventArgs e)
         {
             if (CurrentProject.Folder == null)
@@ -225,19 +247,6 @@ namespace CompanionFormApp
 
             gitCommitForm.ShowDialog();
         }
-
-        private void btnAllTickets_clicked(object sender, EventArgs e)
-        {
-            if (CurrentProject.Tickets.Count == 0) return;
-
-            lstbxProjectTickets.Items.Clear();
-
-            ProjectTickets = CurrentProject.Tickets;
-
-            foreach (var ticket in ProjectTickets)
-            {
-                lstbxProjectTickets.Items.Add(ticket.Name);
-            }
-        }
+        #endregion
     }
 }
